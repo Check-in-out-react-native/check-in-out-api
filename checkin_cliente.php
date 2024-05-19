abd2600a8d192b38d1d5f9c03862ebd21cec1a6b
Freitas86 <129235193+Freitas86@users.noreply.github.com>
2024-05-18 22:22:50 -0300

    Update checkin_cliente.php
    adicionando a transaction de update na tabela de mesas

--- checkin_cliente.php
+++ checkin_cliente.php
@@ -15,6 +15,8 @@ if (isset($_POST['nome_cliente'],$_POST['qtd_pessoas'],$_POST['id_mesa']))
     try {
         $stmt = $conn->prepare("INSERT INTO clientes (nome_cliente, qtd_pessoas, id_mesa, data_ini, espera) VALUES ('$nome_cliente', $qtd_pessoas, $id_mesa, NOW(), 0)");
         $stmt->execute();
+        $stmt1 = $conn->prepare("UPDATE mesas SET reserva = 1 WHERE id_mesa = $id_mesa");
+        $stmt1->execute();
         $clienteId = $conn->lastInsertId();
         echo json_encode(['id_cliente' => $clienteId, 'nome_cliente' => $nome_cliente, 'qtd_pessoas' => $qtd_pessoas, 'id_mesa' => $id_mesa, 'data_ini' => 'data atual', 'espera' =>  0]);
     } catch(PDOException $e) {
