74ec5178fbc76f16fd00b177a379158d70fde44e
Freitas86 <129235193+Freitas86@users.noreply.github.com>
2024-05-25 14:55:09 -0300

    Update listar_mesas.php

--- listar_mesas.php
+++ listar_mesas.php
@@ -6,7 +6,7 @@ header("Content-Type: application/json; charset=UTF-8");
 require 'database.php';
 
 try {
-    $mesa= $conn->query('SELECT * FROM mesas');
+    $mesa= $conn->query('SELECT * FROM mesas A LEFT JOIN clientes B ON B.id_mesa = A.id_mesa');
     $rs = $mesa->fetchAll(PDO::FETCH_ASSOC);
     header('Content-Type: application/json');
     echo json_encode($rs);
